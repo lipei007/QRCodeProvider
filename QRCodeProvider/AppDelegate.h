7a39bdc443264109861a3a3ebcad23756113d69c
mini1 <macmini1@mini1deMac-mini.local>
2016-11-21 11:58:57 +0800

    Initial Commit

---
+++ QRCodeProvider/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCodeProvider
+//
+//  Created by Jack on 2016/11/21.
+//  Copyright © 2016年 mini1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCodeProvider/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCodeProvider
+//
+//  Created by Jack on 2016/11/21.
+//  Copyright © 2016年 mini1. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
